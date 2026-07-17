02af99470b33d251d9ba1c317e040a0473cdd710
qwer <58898485+khuiqel@users.noreply.github.com>
2026-07-16 20:45:23 -0700

    Even more CLI

--- _source/main_cli.cpp
+++ _source/main_cli.cpp
@@ -43,18 +43,20 @@ int main(int argc, char** argv) {
 	// app.set_help_all_flag("-h,--help", "Print this help message and exit");
 	CLI::App* subcom_archive    = app.add_subcommand("archive",    "Create Q4B Archives");
 	CLI::App* subcom_unpack     = app.add_subcommand("unpack",     "Unpack Q4B Archives");
+	// CLI::App* subcom_patch      = app.add_subcommand("patch",      "Patch (partial overwrite) Q4B Archives");
 	CLI::App* subcom_inspect    = app.add_subcommand("inspect",    "Inspect Q4B Archives");
 	CLI::App* subcom_compress   = app.add_subcommand("compress",   "Single file compression");
 	CLI::App* subcom_decompress = app.add_subcommand("decompress", "Single file decompression");
 	app.require_subcommand(1, 1);
 
 	subcom_archive->add_option("input_file", "Input file (format: file scheme level, on each line)")->required();
-	subcom_archive->add_option("-o", "Output folder");
+	subcom_archive->add_option("output_archive", "Output Q4B archive")->required();
 
+	subcom_unpack->add_option("input_archive", "Input Q4B archive")->required();
 	subcom_unpack->add_option("input_file", "Input file (format: file, on each line)")->required(); //TODO: list of files OR single file (-f?)
 	subcom_unpack->add_option("-o", "Output folder");
 
-	subcom_inspect->add_option("input_file", "Input file")->required();
+	subcom_inspect->add_option("input_archive", "Input Q4B archive")->required();
 	subcom_inspect->add_flag("-c", "Calculate and verify metadata"); //TODO
 
 	subcom_compress->add_option("input_file", "Input file")->required();
@@ -74,24 +76,35 @@ int main(int argc, char** argv) {
 	auto* subcom = app.get_subcommands()[0];
 	if (subcom == subcom_archive) {
 
-		const std::string input = subcom_archive->get_option_no_throw("input_file")->as<std::string>();
-		const std::string output = subcom_archive->get_option_no_throw("-o")->empty() ?
-		                           "." : subcom_archive->get_option_no_throw("-o")->as<std::string>();
+		const std::string FILES      = subcom_archive->get_option_no_throw("input_file")->as<std::string>();
+		const std::string ARCHIVE    = subcom_archive->get_option_no_throw("output_archive")->as<std::string>();
 
 		std::vector<q4b::CompressionFile> file_list;
-		ReadArchiveInputFile(input, file_list);
+		ReadArchiveInputFile(FILES, file_list);
 
 		std::vector<q4b::ErrorMessage> messages;
-		q4b::WriteArchive(file_list, ".", std::filesystem::path(output) / "test.q4b", 4, &messages);
+		q4b::WriteArchive(file_list, ".", ARCHIVE, 4, &messages);
 
 	} else if (subcom == subcom_unpack) {
-		//TODO
+
+		const std::string ARCHIVE    = subcom_archive->get_option_no_throw("input_archive")->as<std::string>();
+		const std::string FILES      = subcom_archive->get_option_no_throw("input_file")->as<std::string>();
+		const std::string OUTPUT_DIR = subcom_archive->get_option_no_throw("-o")->empty() ?
+		                               "." : subcom_archive->get_option_no_throw("-o")->as<std::string>();
+
+		std::vector<q4b::CompressionFile> file_list;
+		ReadArchiveInputFile(FILES, file_list);
+
+		q4b::DecodeArchive(ARCHIVE, OUTPUT_DIR);
+
 	} else if (subcom == subcom_inspect) {
 
-		const std::string input = subcom_inspect->get_option_no_throw("input_file")->as<std::string>();
+		const std::string ARCHIVE = subcom_inspect->get_option_no_throw("input_archive")->as<std::string>();
+
 		q4b::ArchiveHeader archiveHeader;
 		std::vector<q4b::ArchivedFileHeader> fileList;
-		q4b::ReadArchiveHeader(input, archiveHeader, fileList);
+		q4b::ReadArchiveHeader(ARCHIVE, archiveHeader, fileList);
+
 		std::cout << archiveHeader.magic << " "
 		          << archiveHeader.flags << " "
 		          << archiveHeader.version << " "
@@ -108,16 +121,16 @@ int main(int argc, char** argv) {
 
 	} else if (subcom == subcom_compress) {
 
-		const std::string input = subcom_compress->get_option_no_throw("input_file")->as<std::string>();
-		const std::string scheme = subcom_compress->get_option_no_throw("scheme")->as<std::string>();
-		const std::string level = subcom_compress->get_option_no_throw("level")->as<std::string>();
-		const std::string output = subcom_compress->get_option_no_throw("-o")->empty() ?
-		                           "." : subcom_compress->get_option_no_throw("-o")->as<std::string>();
-		const bool write_metadata = subcom_compress->get_option_no_throw("-m")->empty();
+		const std::string INPUT      = subcom_compress->get_option_no_throw("input_file")->as<std::string>();
+		const std::string SCHEME     = subcom_compress->get_option_no_throw("scheme")->as<std::string>();
+		const std::string LEVEL      = subcom_compress->get_option_no_throw("level")->as<std::string>();
+		const std::string OUTPUT_DIR = subcom_compress->get_option_no_throw("-o")->empty() ?
+		                               "." : subcom_compress->get_option_no_throw("-o")->as<std::string>();
+		const bool WRITE_METADATA = subcom_compress->get_option_no_throw("-m")->empty();
 
-		// if (std::filesystem::exists(input) && std::filesystem::is_directory(output)) {
+		// if (std::filesystem::exists(INPUT) && std::filesystem::is_directory(OUTPUT_DIR)) {
 		char* file_data;
-		const int64_t file_size = q4b::LoadFileIntoMemory(input, &file_data);
+		const int64_t file_size = q4b::LoadFileIntoMemory(INPUT, &file_data);
 		if (file_size == -1) {
 			std::cout << "ERROR: file not found\n";
 			return 1;
@@ -125,21 +138,21 @@ int main(int argc, char** argv) {
 
 		//TODO
 		char* compressed_file_data;
-		size_t compressed_size = q4b::CompressLz4Data(file_data, std::filesystem::file_size(input), &compressed_file_data, std::stoi(level));
+		size_t compressed_size = q4b::CompressLz4Data(file_data, std::filesystem::file_size(INPUT), &compressed_file_data, std::stoi(LEVEL));
 
-		WriteFile((output / std::filesystem::path(input).filename()).string() + SchemeToExtension(q4b::CompressionScheme::lz4, write_metadata), compressed_file_data, compressed_size);
+		WriteFile((OUTPUT_DIR / std::filesystem::path(INPUT).filename()) / SchemeToExtension(q4b::CompressionScheme::lz4, WRITE_METADATA), compressed_file_data, compressed_size);
 		delete[] file_data;
 
 	} else if (subcom == subcom_decompress) {
 
 		/*
-		const std::string input = subcom_decompress->get_option_no_throw("input_file")->as<std::string>();
-		const std::string scheme = ""; //TODO
-		const std::string output = subcom_decompress->get_option_no_throw("-o")->empty() ?
-		                           "." : subcom_decompress->get_option_no_throw("-o")->as<std::string>();
+		const std::string INPUT      = subcom_decompress->get_option_no_throw("input_file")->as<std::string>();
+		const std::string SCHEME     = subcom_decompress->get_option_no_throw("scheme")->as<std::string>();
+		const std::string OUTPUT_DIR = subcom_decompress->get_option_no_throw("-o")->empty() ?
+		                               "." : subcom_decompress->get_option_no_throw("-o")->as<std::string>();
 
 		char* file_data;
-		const int64_t file_size = q4b::LoadFileIntoMemory(input, &file_data);
+		const int64_t file_size = q4b::LoadFileIntoMemory(INPUT, &file_data);
 		if (file_size == -1) {
 			std::cout << "ERROR: file not found\n";
 			return 1;
@@ -147,15 +160,15 @@ int main(int argc, char** argv) {
 
 		char* decompressed_file;
 		size_t decompressed_size;
-		if (std::filesystem::path(input).extension() == ".lz4f") {
+		if (std::filesystem::path(INPUT).extension() == ".lz4f") {
 			decompressed_size = q4b::DecompressLz4Data_Metadata(file_data, file_size, &decompressed_file, uncompressed_size);
-		} else if (std::filesystem::path(input).extension() == ".lz4") {
+		} else if (std::filesystem::path(INPUT).extension() == ".lz4") {
 			decompressed_size = q4b::DecompressLz4Data(file_data, file_size, &decompressed_file, uncompressed_size);
 		} else {
 			//TODO
 		}
 
-		WriteFile((output / std::filesystem::path(input).stem()).string(), decompressed_file, uncompressed_size);
+		WriteFile((OUTPUT_DIR / std::filesystem::path(INPUT).stem()).string(), decompressed_file, uncompressed_size);
 		*/
 
 	} else {
